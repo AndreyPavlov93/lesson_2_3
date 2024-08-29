a45c6153e67f0069d62159510ba35e37421cc57f
AndreyPavlov93 <grelllok@mail.ru>
2024-08-29 23:04:59 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,14 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-while 0 in my_list: my_list.remove(0)
 index = 0
 while index < len(my_list):
     if my_list[index] >= 0:
         print(my_list[index])
         index += 1
+        if my_list[index]<0:
+            break
+
+
+
 
 
 
